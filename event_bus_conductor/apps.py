3042e4e5dabed3d3a176b25167a64d582c624600
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2024-02-19 12:20:40 +0200

    Initial commit

---
+++ event_bus_conductor/apps.py
@@ -0,0 +1,65 @@
+"""
+App configuration for event_bus_conductor.
+"""
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+from openedx.core.djangoapps.plugins.constants import (
+    ProjectType, SettingsType, PluginURLs, PluginSettings
+)
+
+
+EXTENSIONS_APP_NAME = 'event_bus_conductor'
+
+
+class EventBusConductorPluginConfig(AppConfig):
+    """
+    Event bus conductor configuration.
+    """
+    name = EXTENSIONS_APP_NAME
+    verbose_name = 'Event bus conductor'
+
+    # Class attribute that configures and enables this app as a Plugin App.
+    plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: EXTENSIONS_APP_NAME,
+                PluginURLs.APP_NAME: EXTENSIONS_APP_NAME,
+                PluginURLs.REGEX: r'^event_bus_conductor/',
+                PluginURLs.RELATIVE_PATH: 'urls',
+            },
+            ProjectType.CMS: {
+                PluginURLs.NAMESPACE: EXTENSIONS_APP_NAME,
+                PluginURLs.APP_NAME: EXTENSIONS_APP_NAME,
+                PluginURLs.REGEX: r'^event_bus_conductor/',
+                PluginURLs.RELATIVE_PATH: 'urls',
+            }
+        },
+
+        PluginSettings.CONFIG: {
+            ProjectType.LMS: {
+                SettingsType.COMMON: {
+                    PluginSettings.RELATIVE_PATH: 'settings.common',
+                },
+                SettingsType.TEST: {
+                    PluginSettings.RELATIVE_PATH: 'settings.test',
+                },
+                SettingsType.PRODUCTION: {
+                    PluginSettings.RELATIVE_PATH: 'settings.production',
+                },
+            },
+            ProjectType.CMS: {
+                SettingsType.COMMON: {
+                    PluginSettings.RELATIVE_PATH: 'settings.common',
+                },
+                SettingsType.TEST: {
+                    PluginSettings.RELATIVE_PATH: 'settings.test',
+                },
+                SettingsType.PRODUCTION: {
+                    PluginSettings.RELATIVE_PATH: 'settings.production',
+                },
+            },
+        }
+    }
