18c2fa94f72f842e1ef0b2a1a3fc761b078757fe
wowkalucky <wowkalucky@gmail.com>
2024-04-10 13:10:36 +0300

    fix: add testing plugin settings

--- event_bus_conductor/settings/test.py
+++ event_bus_conductor/settings/test.py
@@ -66,3 +66,12 @@ TEMPLATES = [
 ]
 
 LANGUAGE_CODE = "en"
+
+
+# pylint: disable=unnecessary-pass,unused-argument
+def plugin_settings(settings):
+    """
+    Set of plugin settings used by the Open Edx platform.
+    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    """
+    pass
